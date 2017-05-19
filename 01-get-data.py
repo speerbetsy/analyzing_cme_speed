78b82f4ab1099f76ff5353c61e25b47b360d51f2
Shane Maloney <shane.maloney@tcd.ie>
2017-05-19 11:45:35 +0100

    Updates

--- 01-get-data.py
+++ 01-get-data.py
@@ -1,35 +1,28 @@
 from bs4 import BeautifulSoup
 from urllib import request
 import re
-import pandas as pd
 import os
 
-base_data_path = 'data'
-base_url = 'http://cdaw.gsfc.nasa.gov/CME_list/'
+BASE_DATA_PATH = 'data'
+BASE_URL = 'http://cdaw.gsfc.nasa.gov/CME_list/'
 
-r = request.urlopen(base_url).read()
+r = request.urlopen(BASE_URL).read()
 soup = BeautifulSoup(r, 'html.parser')
 
 table = soup.find('table')
 
 year_month_links = table.find_all('a', href=True)
 
-
-def parse_yht(data):
-    return pd.read_csv(data)
-
-
 for link in year_month_links:
     path_url = '/'.join(link.get('href').split('/')[:-1]) + '/'
-    r = request.urlopen(base_url+link.get('href')).read()
+    r = request.urlopen(BASE_URL + link.get('href')).read()
     soup = BeautifulSoup(r, 'html.parser')
     events = soup.find_all(href=re.compile("yht"))
     for event in events:
         file_name = event.get('href').split('/')[-1]
-        file_path = base_data_path+'/raw/' + file_name
+        file_path = BASE_DATA_PATH + '/raw/' + file_name
 
-        raw_data = request.urlopen(base_url+path_url+event.get('href')).read()
+        raw_data = request.urlopen(BASE_URL + path_url + event.get('href')).read()
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, 'wb') as file:
             file.write(raw_data)
-        # data = parse_yht(raw_data)
--- 02-prep-data.py
+++ 02-prep-data.py
@@ -1,12 +1,33 @@
 import pandas as pd
 from os import listdir
+from io import StringIO
 
-base_data_path = 'data/raw/'
+BASE_DATA_PATH = 'data/raw/'
+COLUMNS = ('DATE-OBS', 'TIME-OBS', 'DETECTOR', 'FILTER', 'OBSERVER', 'FEAT_CODE', 'IMAGE_TYPE', 'YHT_ID',
+           'ORIG_HTFILE', 'ORIG_WDFILE', 'UNIVERSAL', 'WDATA', 'WDATA', 'HALO', 'ONSET1', 'ONSET2', 'ONSET2',
+           'CEN_PA', 'WIDTH', 'SPEED', 'ACCEL', 'FEAT_PA', 'FEAT_QUAL', 'QUALITY_INDEX', 'REMARK', 'COMMENT',
+           'NUM_DATA_POINTS', 'HT_DATA')
 
-files = listdir(base_data_path)
+file_names = listdir(BASE_DATA_PATH)
 
-for file in files:
-     data = pd.read_csv('data/raw/'+files[-1],
-            sep='\s+',  comment='#', 
-            names=['height', 'date', 'time', 'angle', 'tel', 'fc', 'col', 'row'],
-            parse_dates=[[1,2]])
+df = pd.DataFrame(columns=COLUMNS)
+
+for file_name in file_names:
+    with open(BASE_DATA_PATH + file_name) as file:
+        lines = file.readlines()
+        meta_data = lines[1:27]
+        meta_data = [element.split(': ')[1][:-1] for element in meta_data]
+        height_time_data = lines[27:]
+        data = pd.read_csv(StringIO(''.join(height_time_data)),
+                           sep='\s+', comment='#',
+                           names=['HEIGHT', 'DATE', 'TIME', 'ANGLE', 'TEL', 'FC', 'COL', 'ROW'],
+                           parse_dates=[[1, 2]])
+
+        meta_data.append(len(data))
+        meta_data.append(data)
+        cme_count = len(df)
+        df.loc[cme_count] = meta_data
+        if cme_count % 1000 == 0:
+            print(cme_count)
+
+df.to_pickle('all_cmes.pkl')
