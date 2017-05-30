a66783477dd0d4d1faf4b0880548de065e81733c
espeer <elizabeth.speer@ucdconnect.ie>
2017-05-30 11:37:37 +0100

    may 30 11:38

--- functions/cme_graphing.py
+++ functions/cme_graphing.py
@@ -20,7 +20,7 @@ def find_file(date_time):
         x+=1
     return (day)
 
-def height_velocity_graphs(day, desc):
+def fit_to_function_height(day):
     #Extracting data
     height_data=day['HEIGHT']
     time_data=day['DATE_TIME']
@@ -28,35 +28,57 @@ def height_velocity_graphs(day, desc):
     #Making calculations
     t=time_data-time_data[0]
     t=(np.array(t, dtype=datetime.datetime))*(10**-9)/60
+    #get centroid point
+    diff_t=np.diff(t)
+    center_t=t[:-1]+(0.5*diff_t)
+    return center_t, height_data
+
+def format_time(time):
+    #Making calculations
+    t=time-time[0]
+    t=(np.array(t, dtype=datetime.datetime))*(10**-9)/60
+    #get centroid point
+    diff_t=np.diff(t)
+    center_t=t[:-1]+(0.5*diff_t)
+    return (center_t)
+
+def get_derivative(y,x):
+    diff_y=np.diff(y)
+    diff_x=np.diff(x)
+    derivative=diff_y/((diff_x.astype(float))*10**-9)
+    return (derivative)
+
+def fit_to_function_velocity(day):
+   return 0
     
+def height_graphs(x, y, desc):
     #Height vs time (Rsun)
     plt.title("Height (Rsun) vs Time "+desc)
     plt.xlabel('Time')
     plt.ylabel('Height (Rsun)')
-    plt.plot(t, height_data,'+')
-    plt.gcf().autofmt_xdate()
+    plt.plot(x, y,'+')
+    #plt.gcf().autofmt_xdate()
     plt.show()
     
     #Height vs Time (km)
-    height_data_km=np.array(height_data)
-    height_data_km=height_data_km*695000
+    y_km=(np.array(y))*695000
     plt.title("Height (km) vs Time "+desc)
     plt.xlabel('Time')
     plt.ylabel('Height (km)')
-    plt.plot(t, height_data_km,'+')
-    plt.gcf().autofmt_xdate()
+    plt.plot(x,y_km,'+')
+    #plt.gcf().autofmt_xdate()
     plt.show()
-    
-    #calcuations for velocity
-    diff_height_data=np.diff(height_data_km)
-    diff_time_data=np.diff(time_data)
-    velocity=diff_height_data/((diff_time_data.astype(float))*10**-9)
-    
+ 
+def velocity(time, v_0, a_0):
+    #Velocity 1: Model equation
+    return (v_0 + (time * a_0))
+
+def velocity_graphs(x,y, desc):
     #Velocity vs Time
     plt.title("Velocity vs Time " +desc)
     plt.xlabel('Time (min)')
     plt.ylabel('Velocity km/sec')
-    plt.plot(t[1:].astype(float), velocity,'+') #one less data point
+    plt.plot(x, y,'+') #one less data point
     plt.show()
 
    
